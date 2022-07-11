6a09caf6d4f294c1b2b284108435635f33a6b63c
LeThanh <kzxlmdtn888@gmail.com>
2022-07-11 10:50:53 +0700

    ..

--- Handling-Logging-Error/Winston-in-ExpressJS/index.ts
+++ Handling-Logging-Error/Winston-in-ExpressJS/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 
 import bodyParser from "body-parser";
 
-import { logger } from "./src/logger/winston"
+import { logger } from "./src/logger/winston";
 
+import * as Sentry from "@sentry/node";
 
+import * as Tracing from "@sentry/tracing";
 
 const PORT = 3000;
 
@@ -12,7 +14,35 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+Sentry.init({
+
+    dsn: "https://dcbbefa1081c407ea672433d6824fbe3@o1313393.ingest.sentry.io/6563495",
+
+    integrations: [
+
+        new Sentry.Integrations.Http({ tracing: true }),
+
+        new Tracing.Integrations.Express({ app }),
+
+    ],
+
+
+
+    tracesSampleRate: 1.0,
+
+});
+
+
+
+app.use(Sentry.Handlers.requestHandler());
+
+app.use(Sentry.Handlers.tracingHandler());
+
+
+
+
+
+app.get('/', (req, res,next) => {
 
     try {
 
@@ -21,13 +51,30 @@ app.get('/', (req, res) => {
         throw new Error("Error test winston");
 
     } catch (err) {
+        next(err);
 
         logger.error(err);
 
     }
 
 })
+app.use(Sentry.Handlers.errorHandler());
 
+app.use(function onError(err, req, res, next) {
+
+    res.statusCode = 500;
+
+    res.end(res.sentry + "\n");
+
+});
+
+
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+
+    throw new Error("My first Sentry error!");
+
+});
 
 
 
